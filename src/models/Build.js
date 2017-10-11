03bdb91a6ff79626b986f442388cb09ddc556736
duxet <duxetlg@gmail.com>
2017-10-11 11:25:47 +0200

    Fix CircleCI build status

--- src/models/Build.js
+++ src/models/Build.js
@@ -4,6 +4,6 @@ export default class Build {
   }
 
   get isSuccessful() {
-    return ['passed', 'fixed'].includes(this.status.name)
+    return ['fixed', 'success', 'passed'].includes(this.status.name)
   }
 }
